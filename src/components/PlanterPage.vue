a045cc55785c5c7727946084e12492c3138c4c9b
Ingo Steinke <ingosteinke@gmx.de>
2021-02-27 13:46:35 +0100

    add component for personal info

---
+++ src/components/PlanterPage.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <main class="planterpage">
+    <h1>Planted</h1>
+    <p>Annika Muster, planter since January 2021</p>
+  </main>
+</template>
+
+<script lang="ts">
+import defineComponent from "vue"
+
+export default defineComponent({
+  name: "PlanterPage",
+  props: {},
+})
+</script>
+
+<style scoped lang="scss">
+  .planterpage {
+    background: $planted-background;
+    color: $planted-gray;
+  }
+</style>

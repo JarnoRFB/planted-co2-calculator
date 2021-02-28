68f5e88e3ad464e0f8f7ee74fabe516b3bf22e33
rbusche <rbusche@inserve.de>
2021-02-28 08:57:30 +0100

    Make pwa

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
 import i18n from './i18n'
+import './registerServiceWorker'
 
 const app = createApp(App).use(i18n)
 installElementPlus(app)

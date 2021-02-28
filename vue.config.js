499627994f973f9698ae940655d9e98be10d0b77
rbusche <rbusche@inserve.de>
2021-02-28 09:31:15 +0100

    Adapt config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
       enableLegacy: false,
       runtimeOnly: false,
       compositionOnly: false,
-      fullInstall: true
-    }
-  }
+      fullInstall: true,
+    },
+    pwa: {
+      name: "planted",
+      themeColor: "#bfb8a3",
+      appleMobileWebAppCapable: "yes",
+    },
+  },
 }
